6a143e9e7adacb713c0a4652d82afc8547154746
adihoo <hoode.aditya@yahoo.com>
2021-01-20 19:41:28 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int diceOneValue = 1;
+  int diceTwoValue = 1;
+
+  void changeFace() {
+    setState(() {
+      diceOneValue = Random().nextInt(6) + 1;
+      diceTwoValue = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeFace();
+              },
+              child: Image.asset('images/dice$diceOneValue.png'),
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeFace();
+              },
+              child: Image.asset('images/dice$diceTwoValue.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
